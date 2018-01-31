aac6550baedb84a27a7d41104bc818da9031e327
Tlenc <tlenciauskas@gmail.com>
2018-01-31 10:21:14 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Įveskite skaičių");
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	if(a%2==0){
+	    System.out.println("Skaičius yra lyginis");
+    }else{
+	        System.out.println("Skaičius yra nelyginis");
+        }
     }
 }
