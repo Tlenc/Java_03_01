0ced3905634228be07fabbbac91c30e8c4394153
Tlenc <tlenciauskas@gmail.com>
2018-01-31 10:39:59 +0200

    SetGet

--- src/Uzdaviniai/LyginisNelyginis.java
+++ src/Uzdaviniai/LyginisNelyginis.java
@@ -1,13 +1,21 @@
 package Uzdaviniai;
 
-import java.util.Scanner;
 
 public class LyginisNelyginis {
-    public LyginisNelyginis(){
-        System.out.println("Įveskite skaičių");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if (a % 2 == 0) {
+    private int _a;
+
+    public int get_a() {
+        return _a;
+    }
+
+    public void set_a(int value) {
+        _a = value;
+
+    }
+
+    public void skaiciuok() {
+
+        if (_a % 2 == 0) {
             System.out.println("Skaičius yra lyginis");
         } else {
             System.out.println("Skaičius yra nelyginis");
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,17 @@ import Uzdaviniai.LyginisNelyginis;
 
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Įveskite skaičių");
+        Scanner sc = new Scanner(System.in);
+        int _a = sc.nextInt();
+
         LyginisNelyginis objektas = new LyginisNelyginis();
+        objektas.set_a(_a);
+        System.out.println("Skaičius : " + objektas.get_a());
+        objektas.skaiciuok();
     }
 }
